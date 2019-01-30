8cbc4d6ffa165d3e2dda44909a123e0466e5db9d
Vivek Sharma <vivekssharma07@gmail.com>
2019-01-30 17:59:01 +0530

    Database

--- config/database.php
+++ config/database.php
@@ -88,7 +88,7 @@ return [
 
         'mongodb' => [
             'driver'   => 'mongodb',
-            'host'     => env('MONGO_DB_HOST', 'localhost'),
+            'host'     => env('MONGO_DB_HOST', '127.0.0.1'),
             'port'     => env('MONGO_DB_PORT', 27017),
             'database' => env('MONGO_DB_DATABASE','taskrem'),
             'username' => env('MONGO_DB_USERNAME'),
